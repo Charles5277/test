3b9615ca9666ba3c691a75804cc364a5cd07c5f3
楊鈞元 <abcd854884@gmail.com>
2023-01-25 07:52:55 +0000

    Update main.js and vuetify.js

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,60 +1,60 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
+// import Vue from 'vue';
+// import Vuetify from 'vuetify';
+// import 'vuetify/dist/vuetify.min.css';
 
-import colors from 'vuetify/lib/util/colors';
-import VueI18n from 'vue-i18n';
+// import colors from 'vuetify/lib/util/colors';
+// import VueI18n from 'vue-i18n';
 
-import en from '@/../locales/en.json';
-import tw from '@/../locales/tw.json';
+// import en from '@/../locales/en.json';
+// import tw from '@/../locales/tw.json';
 
-Vue.use(Vuetify);
-Vue.use(VueI18n);
+// Vue.use(Vuetify);
+// Vue.use(VueI18n);
 
-const messages = {
-  en: en,
-  tw: tw,
-};
+// const messages = {
+//   en: en,
+//   tw: tw,
+// };
 
-// 檢測瀏覽器語言
-let browserLanguage = window.navigator.language;
+// // 檢測瀏覽器語言
+// let browserLanguage = window.navigator.language;
 
-if (browserLanguage.toLowerCase() === 'en') {
-  browserLanguage = 'en';
-} else {
-  browserLanguage = 'tw';
-}
+// if (browserLanguage.toLowerCase() === 'en') {
+//   browserLanguage = 'en';
+// } else {
+//   browserLanguage = 'tw';
+// }
 
-// 實例化 vuei18n
-const i18n = new VueI18n({
-  locale: browserLanguage,
-  messages,
-});
+// // 實例化 vuei18n
+// const i18n = new VueI18n({
+//   locale: browserLanguage,
+//   messages,
+// });
 
-const opts = {
-  theme: {
-    dark: false,
-    themes: {
-      light: {
-        primary: '#409eff',
-        secondary: '#54a8ff',
-        accent: '#9c27b0',
-        error: '#f44336',
-        warning: '#ff5722',
-        info: '#607d8b',
-        success: '#4caf50',
-      },
-      dark: {
-        primary: colors.blue.darken1,
-      },
-    },
-  },
-  // vuetify 設定中增加 i18n lang
-  lang: {
-    t: (key, ...params) => i18n.t(key, params),
-  },
-};
+// const opts = {
+//   theme: {
+//     dark: false,
+//     themes: {
+//       light: {
+//         primary: '#409eff',
+//         secondary: '#54a8ff',
+//         accent: '#9c27b0',
+//         error: '#f44336',
+//         warning: '#ff5722',
+//         info: '#607d8b',
+//         success: '#4caf50',
+//       },
+//       dark: {
+//         primary: colors.blue.darken1,
+//       },
+//     },
+//   },
+//   // vuetify 設定中增加 i18n lang
+//   lang: {
+//     t: (key, ...params) => i18n.t(key, params),
+//   },
+// };
 
-const vuetify = new Vuetify(opts);
-// 導出 i18n
-export { vuetify, i18n };
+// const vuetify = new Vuetify(opts);
+// // 導出 i18n
+// export { vuetify, i18n };
